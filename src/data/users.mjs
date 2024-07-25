94974db2a94f4d31ebafb2d730f7a3d9cb47c04c
QaseemB <Qbarnhardt@yahoo.com>
2024-07-24 23:46:42 -0400

    sba319 mongodb insertmanytimeout

--- src/data/users.mjs
+++ src/data/users.mjs
@@ -1,47 +1,66 @@
-const users = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import { USERS } from "../models/users.mjs";
+
+
+
+
+const users =  async () => {
+   try{
+    
+    const userdb = [
+
     {
-      id: 1,
+ 
       name: "Carey",
       username: "cyare23",
       password: "ellogov",
       email: "cy23@example.com",
     },
     {
-      id: 2,
+
       name: "Mikoto",
       username: "MKultra",
       password: "buddawearsgucci",
       email: "mikoto_u@example.com",
     },
     {
-      id: 3,
+
       name: "Ronald",
       password: "michealmeyers",
       username: "RonRonRon",
       email: "mronald@example.com",
     },
     {
-      id: 4,
+
       name: "lamine",
       password: "sunsetdrive69",
       username: "bestofthebest",
       email: "sweet16@gmail.com"
     },
     {
-      id: 5,
+
       name: "messi",
       username: "Goat10",
       password: "c05tompa55w0rd",
       email: "barcalegend@gmail.com"
     },
     {
-      id: 6,
       name: "ronaldo",
       username: "bestofthebest",
       password: "aspiringsoftwaredev",
       email: "cr7@gmail.com"
     }
   ];
+      await USERS.insertMany(userdb);
+      console.log('userdb has been saved')
+     }catch (error){
+    console.error('Error saving users', error)
+  }
+  };
+
+  await users();
 
 
 export {users}
